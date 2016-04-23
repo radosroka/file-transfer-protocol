81e8a4321dc46bfba6540bc73c08238fedf5dd36
Radovan Sroka <rsroka@redhat.com>
2016-04-23 21:34:20 +0200

    Something works

--- client.cpp
+++ client.cpp
@@ -131,56 +131,89 @@ int main(int argc, char **argv){
 		cout << "Start UPLOADING" << endl;
 		long file_size = GetFileSize(params.what);
 		if (file_size == -1){
+			close(socketfd);
 			cerr << "File doesn't exist" << endl;
 			return EXIT_FAILURE;
 		}
 		cout << "FILE_SIZE is: " << file_size << endl;
 
-		int file = open(params.what.c_str(), O_RDONLY);
-		if (file == -1){
-			cerr << "File " << params.what << " doesn't exist" << endl;
-			return EXIT_FAILURE;
-		}
-	
-		for (long i = 0 ; i <= file_size/BUFFER_SIZE ; i++){
-			char from_file[BUFFER_SIZE]; 						
-			memset(from_file, 0, BUFFER_SIZE);
-			int bytes = read(file, from_file, BUFFER_SIZE);
-			if (bytes == -1){
-				cerr << "read error!" << endl ;
-				close(file);
-				return EXIT_FAILURE;
-			}
+		char * data = new char[file_size];
+		ifstream f;
+		f.open(params.what.c_str(), ios::in | ios::binary);
+		f.read(data, file_size);
+		f.close();
+		string msg = "UPLOAD:" + params.what + ";" + "FILE_SIZE:" + to_string(file_size) + 
+					 ";" + "PART_NUM:" + "aaa" + ";" + "PART_SIZE:" + "aaa" + ";\n";
+		int msg_size = msg.length();
+		msg.append(data, file_size);
+		send(socketfd, msg.c_str(), msg_size + file_size, 0);
+		delete [] data;
 
-			string msg = "UPLOAD:" + params.what + ";" + "FILE_SIZE:" + to_string(file_size) + 
-						 ";" + "PART_NUM:" + to_string(i) + ";" + "PART_SIZE:" + to_string(bytes) + ";\n";
-			int msg_size = msg.length() + bytes;
-			msg.append(from_file, bytes);
-			cout << "--------------------------------------------------------------------------------------" << endl;
-			cout << msg;
-			cout << "--------------------------------------------------------------------------------------" << endl;
-			send(socketfd, msg.c_str(), msg_size, 0);
-		}
-		close(file);
-	}
-	else {/*
 		int bytes;
 		long bytes_received = 0;
-		string data = "";
-		char incoming_data_buffer[BUFFER_SIZE];		 
+		string rcv = "";
+		char incoming_data_buffer[BUFFER_SIZE];
 		memset(incoming_data_buffer, 0, BUFFER_SIZE);
-	
+
 		while ((bytes = recv(socketfd, incoming_data_buffer, BUFFER_SIZE, 0))){
 			bytes_received += bytes;
 			if (bytes == -1){
 				cerr << "recieve error!" << endl ;
 				close(socketfd);
-				freeaddrinfo(host_info_list);
+				return EXIT_FAILURE;
+			}
+			else rcv.append(incoming_data_buffer, bytes);
+			memset(incoming_data_buffer, 0, BUFFER_SIZE);
+		}
+
+		if (rcv.find("ERROR") != string::npos){
+			cerr << "file: " << params.what.c_str() << " cannot upload" << endl ;
+			close(socketfd);
+			return EXIT_FAILURE;
+		}
+	}
+	else {
+		string msg = "DOWNLOAD:" + params.what + ";\n";
+		cout << msg << endl;
+		send(socketfd, msg.c_str(), msg.length(), 0);
+		shutdown(socketfd, SHUT_WR);
+		cout << "after send" << endl;
+		int bytes;
+		long bytes_received = 0;
+		string data = "";
+		char incoming_data_buffer[BUFFER_SIZE];
+		memset(incoming_data_buffer, 0, BUFFER_SIZE);
+		
+		while ((bytes = recv(socketfd, incoming_data_buffer, BUFFER_SIZE, 0))){
+			bytes_received += bytes;
+			if (bytes == -1){
+				cerr << "recieve error!" << endl ;
+				close(socketfd);
 				return EXIT_FAILURE;
 			}
 			else data.append(incoming_data_buffer, bytes);
 			memset(incoming_data_buffer, 0, BUFFER_SIZE);
 		}
-*/	}
+
+		cout << "after recv" << endl;
+
+		string header = data.substr(0, data.find(";\n"));
+		string body = data.substr(data.find(";\n") + 2);
+
+		if (header.find("ERROR") != string::npos){
+			cerr << "file: " << params.what.c_str() << " doesn't exist" << endl ;
+			close(socketfd);
+			return EXIT_FAILURE;
+		}
+		else if (header.find("DOWNLOAD:") != string::npos){
+			int offset = 9;
+			string file_name = header.substr(offset, header.find_first_of(";", offset) - offset);
+			cout << file_name << endl;
+			ofstream f;
+			f.open(file_name + "2", ios::out | ios::binary);
+			f << body;
+			f.close();
+		}
+	}
 	return EXIT_SUCCESS;
 }
--- server.cpp
+++ server.cpp
@@ -8,92 +8,185 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <pthread.h>
+#include <sys/stat.h>
 
-#define PORT	"8000"
+#include <fstream>
+
 #define BACKLOG	 50
 
 using namespace std;
 
 const int BUFFER_SIZE = 2048;
 
-void * handle(void *pnewsock)
-{
-	/* send(), recv(), close() */
+long GetFileSize(string filename){
+	struct stat stat_buf;
+	int rc = stat(filename.c_str(), &stat_buf);
+	return rc == 0 ? stat_buf.st_size : -1;
+}
+
+void * handle(void *pnewsock){
+
 	cout << "I'm in thread" << endl;
 	
 	int bytes;
-		long bytes_received = 0;
-		string data = "";
-		char incoming_data_buffer[BUFFER_SIZE];
-		memset(incoming_data_buffer, 0, BUFFER_SIZE);
-
-		while ((bytes = recv(*(int *)pnewsock, incoming_data_buffer, BUFFER_SIZE, 0))){
-			bytes_received += bytes;
-			if (bytes == -1){
-				cerr << "recieve error!" << endl ;
-				close(*(int*)pnewsock);
-				return NULL;
-			}
-			else data.append(incoming_data_buffer, bytes);
-			memset(incoming_data_buffer, 0, BUFFER_SIZE);
+	long bytes_received = 0;
+	string data = "";
+	char incoming_data_buffer[BUFFER_SIZE];
+	memset(incoming_data_buffer, 0, BUFFER_SIZE);
+	cout << "bfr while recv" << endl;
+	while ((bytes = recv(*(int *)pnewsock, incoming_data_buffer, BUFFER_SIZE, 0))){
+		bytes_received += bytes;
+		if (bytes == -1){
+			cerr << "recieve error!" << endl ;
+			close(*(int*)pnewsock);
+			return NULL;
 		}
+		else data.append(incoming_data_buffer, bytes);
+		memset(incoming_data_buffer, 0, BUFFER_SIZE);
+	}
 
-		cout << data;
+	if (data == ""){
+		close(*(int *)pnewsock);
+		cout << "End of thread" << endl;
+		return NULL;
+	}
 
+	string header = data.substr(0, data.find(";\n"));
+	string body = data.substr(data.find(";\n") + 2);
+	
+	cout << "bfr if" << endl;
+	cout << data << endl;
+	
+	int offset = 0;
+	if (header.find("UPLOAD:") != string::npos){
+		offset = 7;
+		string file_name = header.substr(offset, header.find_first_of(";", offset) - offset);
+		cout << file_name << endl;
+		ofstream f;
+		f.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
+		string msg = "UPLOAD:OK;\n";
+		try{
+			f.open(file_name+"1", ios::out | ios::binary);
+			f << body;
+			f.close();
+		} catch (exception const& e){
+			cerr << "Couldn't write to file " << file_name << endl;
+			msg = "ERROR;\n";
+		}
+		send(*(int *)pnewsock, msg.c_str(), msg.length(), 0);
+	}
+
+
+	else if (header.find("DOWNLOAD:") != string::npos){
+		cout << "DOWNLOAD" << endl;
+		offset = 9;
+		string file_name = header.substr(offset, header.find_first_of(";", offset) - offset);
+		cout << file_name << endl;
+		
+		long file_size = GetFileSize(file_name);
+		if (file_size == -1){
+			cerr << "File doesn't exist" << endl;
+			file_size = 1;
+		}
+		
+		char * fl = new char[file_size];
+		ifstream f;
+		f.exceptions (ifstream::failbit | ifstream::badbit);
+		string msg = "DOWNLOAD:" + file_name + ";" + "FILE_SIZE:" + to_string(file_size) + 
+				 ";" + "PART_NUM:" + "aaa" + ";" + "PART_SIZE:" + "aaa" + ";\n";
+		
+		cout << "bfr try" << endl;
+		try{
+			f.open(file_name, ios::in | ios::binary);
+			f.read(fl, file_size);
+			f.close();
+		} catch (exception const& e){
+			msg = "ERROR;\n";
+			cout << msg;
+			*fl = '\0';
+			file_size = 0;
+		}		
+		
+		cout << "after try" << endl;
+
+		int msg_size = msg.length();
+		msg.append(fl, file_size);
+		send(*(int *)pnewsock, msg.c_str(), msg_size + file_size, 0);
+		delete [] fl;
+	}
+	//sleep(2000);
+	close(*(int *)pnewsock);
+	cout << "End of thread" << endl;
 	return NULL;
 }
 
-int main(void)
-{
+int main(int argc, char **argv){
 	int sock;
 	pthread_t thread;
-	struct addrinfo hints, *res;
-	int reuseaddr = 1; /* True */
+	struct addrinfo hints, *res = NULL;
+	char * PORT = NULL;
+	int reuseaddr = 1;
+	int status = 0;
 
-	/* Get the address info */
+	if (argc != 3){
+		cerr << "There is only one parameter -p as port" << endl;
+		return EXIT_FAILURE;
+	}
+
+	int c;
+	opterr = 0;
+	while ((c = getopt (argc, argv, "p:")) != -1)
+		switch (c){
+			case 'p':
+				PORT = optarg;
+				break;
+ 			default:
+				cerr  << "Unrecognized parameter: " << optarg << endl;
+				return EXIT_FAILURE;
+				break;
+	  	}
+
+	cout << PORT << endl;
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo(NULL, PORT, &hints, &res) != 0) {
-		perror("getaddrinfo");
-		return 1;
+	
+	status = getaddrinfo(NULL, PORT, &hints, &res);
+	if (status != 0){
+		cerr << "getaddrinfo error: " << gai_strerror(status) << endl;
+		return EXIT_FAILURE;
 	}
 
-	/* Create the socket */
 	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 	if (sock == -1) {
-		perror("socket");
-		return 1;
+		cerr << "Couldn't create socket" << endl;
+		return EXIT_FAILURE;
 	}
 
-	/* Enable the socket to reuse the address */
 	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(int)) == -1) {
-		perror("setsockopt");
-		return 1;
+		cerr << "setsockopt: failed" << endl;
+		return EXIT_FAILURE;
 	}
 
-	/* Bind to the address */
 	if (bind(sock, res->ai_addr, res->ai_addrlen) == -1) {
-		perror("bind");
-		return 0;
+		cerr << "Couldn't bind" << endl;
+		return EXIT_FAILURE;
 	}
 
 	freeaddrinfo(res);
 
-	/* Listen */
 	if (listen(sock, BACKLOG) == -1) {
-		perror("listen");
-		return 0;
+		cerr << "Listen: failed" << endl;
+		return EXIT_FAILURE;
 	}
 
-	/* Main loop */
 	while (1) {
 		printf("In while\n");
 		socklen_t size = sizeof(struct sockaddr_in);
 		struct sockaddr_in their_addr;
 		int newsock = accept(sock, (struct sockaddr*)&their_addr, &size);
 		if (newsock == -1) {
-			perror("accept");
+			cerr << "Accept: failed" << endl;
 		}
 		else {
 			printf("Got a connection from %s on port %d\n", 
@@ -106,6 +199,6 @@ int main(void)
 
 	close(sock);
 
-	return 0;
+	return EXIT_SUCCESS;
 }
 
